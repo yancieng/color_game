7a6288c7ec23fbf1e507bc05de990f8e7d588388
Yancie Ng <yancieng@gmail.com>
2022-07-20 10:02:59 +1000

    setup

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Menu from "./main_menu";
+
+function App() {
+  return <Menu />;
+}
+
+export default App;
---
+++ src/game_interface/index.js
@@ -0,0 +1,13 @@
+import GameBoard from "./GameBoard";
+import Controls from "./Controls";
+
+const Interface = ({ size }) => {
+  return (
+    <>
+      <GameBoard />
+      <Controls />
+    </>
+  );
+};
+
+export default Interface;
